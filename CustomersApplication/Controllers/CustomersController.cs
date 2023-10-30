c530ee3b4563b8c0168521defae4a4f0e8f51732
Monika Bloniarz <monika.bloniarz@capgemini.com>
2023-10-30 10:37:25 +0100

    AddCustomerController

--- CustomersApplication/Controllers/CustomersController.cs
+++ CustomersApplication/Controllers/CustomersController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using CustomersApplication.Database;
+using CustomersApplication.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Linq.Expressions;
 
 namespace CustomersApplication.Controllers
 {
@@ -7,6 +10,81 @@ namespace CustomersApplication.Controllers
     [ApiController]
     public class CustomersController : ControllerBase
     {
+        private DatabaseContext _context;
+        public CustomersController(DatabaseContext context)
+        {
+            _context = context;
+        }
 
+        [HttpGet("GetCustomers")]
+        public IActionResult GetCustomers()
+        {
+            try
+            {
+                var customers = _context.Customers.ToList();
+                return Ok(customers);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("GetCustomer/{Id}")]
+        public IActionResult GetCustomer([FromRoute] int id)
+        {
+            try
+            {
+                var customer = _context.Customers.FirstOrDefault(x => x.Id == id);
+                if (customer == null)
+                {
+                    return BadRequest("Customer with that id doesn't exist");
+                }
+                return Ok(customer);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("AddCustomer")]
+        public IActionResult AddCustomer([FromBody] CustomerModel modelRequest)
+        {
+            var customerDB = new CustomerDB();
+            customerDB.Name= modelRequest.Name;
+            customerDB.Surname= modelRequest.Surname;
+
+            try
+            {
+                _context.Customers.Add(customerDB);
+                _context.SaveChanges();
+            } 
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return Ok(customerDB);
+        }
+
+        [HttpDelete("DeleteCustomer/{Id}")]
+        public IActionResult DeleteCustomer([FromRoute] int id)
+        {
+            try
+            {
+                var customer = _context.Customers.FirstOrDefault(x => x.Id == id);
+                if (customer != null)
+                {
+                    _context.Customers.Remove(customer);
+                    _context.SaveChanges();
+                }
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            var customers = _context.Customers.ToList();
+            return Ok(customers);
+        }
     }
 }
